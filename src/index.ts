90885fd0742eff1ced746bf0a587fbc7630c69a3
Hexy32 <sunshine.goody8762@gmail.com>
2022-04-24 23:35:37 -0400

    Added snowpack

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { evaluate, chain } from 'mathjs'
 
-import confetti from 'canvas-confetti';
-
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+console.log(evaluate('2 + (22221) * 1102'))
