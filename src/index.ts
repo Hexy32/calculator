dc1afdf454ce139773a7c142364f3bdb387bb851
Hexy32 <sunshine.goody8762@gmail.com>
2022-04-26 19:21:44 -0400

    Refactored code to be more modualler and make more sense

--- src/index.ts
+++ src/index.ts
@@ -4,82 +4,104 @@ const buttons = document.querySelectorAll('button')
 const screen = document.getElementById('screen') as HTMLDivElement
 const lowScreen = document.getElementById('low-screen') as HTMLDivElement
 
+document.addEventListener('keypress', (e) => {
+  console.log(e.key)
+})
+
 buttons.forEach((item) => {
   item.addEventListener('click', (event) => {
     const button = event.currentTarget as HTMLButtonElement
-    if (button.hasAttribute('data-operator')) {
-      if (lowScreen.innerText === '0' && button.dataset.operator === '-') {
-        lowScreen.textContent = button.innerText
-        return
-      }
-      const lastCharactor = lowScreen.innerText[lowScreen.innerText.length - 1]
-      const lastTwoCharactors =
-        lowScreen.innerText[lowScreen.innerText.length - 2] + lastCharactor
+    manangeNumber(button.innerText)
+  })
+})
 
-      if (lastTwoCharactors !== '--' && button.innerText === '-') {
-        lowScreen.textContent += button.innerText
-        return
-      }
+function manangeNumber(key: string) {
+  //Numbers
+  try {
+    const keyParsed = JSON.parse(key)
 
-      if (
-        lastCharactor === '*' ||
-        lastCharactor === '/' ||
-        lastCharactor === '+' ||
-        lastTwoCharactors === '--'
-      ) {
+    if (keyParsed <= 9) {
+      if (lowScreen.textContent != '0') {
+        lowScreen.textContent += key
+      } else {
+        lowScreen.textContent = key
+      }
+      return
+    }
+  } catch (error) {
+    if (
+      key === '.' &&
+      lowScreen.textContent![lowScreen.textContent!.length - 1] != '.'
+    ) {
+      lowScreen.textContent += key
+    }
+  }
+
+  //Action Keys
+  switch (key) {
+    case 'AC':
+      lowScreen.innerText = '0'
+      screen.innerText = ''
+      break
+    case 'DEL':
+      if (lowScreen.innerText != '0') {
         lowScreen.innerText = lowScreen.innerText.slice(
           0,
           lowScreen.innerText.length - 1
         )
-        lowScreen.textContent += button.innerText
-        return
       }
-      lowScreen.textContent += button.innerText
+      if (lowScreen.innerText === '') {
+        lowScreen.innerText = '0'
+      }
+      break
+    case '=':
+      try {
+        evaluate(lowScreen.innerText)
+        screen.innerText = lowScreen.innerText
+        lowScreen.innerText = evaluate(lowScreen.innerText)
+      } catch (error) {
+        const cache = lowScreen.innerText
+        lowScreen.innerText = 'error'
+        lowScreen.classList.toggle('error')
+        setTimeout(() => {
+          lowScreen.innerText = cache
+          lowScreen.classList.toggle('error')
+        }, 700)
+      }
+      break
+    default:
+      break
+  }
+
+  //Operators
+  if (key === '*' || key === '/' || key === '-' || key === '+') {
+    if (lowScreen.innerText === '0' && key === '-') {
+      lowScreen.textContent = key
       return
     }
-    if (button.hasAttribute('data-number')) {
-      if (lowScreen.textContent != '0') {
-        lowScreen.textContent += button.innerText
-      } else {
-        lowScreen.textContent = button.innerText
-      }
+    const lastCharactor = lowScreen.innerText[lowScreen.innerText.length - 1]
+    const lastTwoCharactors =
+      lowScreen.innerText[lowScreen.innerText.length - 2] + lastCharactor
+
+    if (lastTwoCharactors !== '--' && key === '-') {
+      lowScreen.textContent += key
       return
     }
 
-    switch (button.dataset.action) {
-      case 'AC':
-        lowScreen.innerText = '0'
-        screen.innerText = ''
-        break
-      case 'DEL':
-        if (lowScreen.innerText != '0') {
-          lowScreen.innerText = lowScreen.innerText.slice(
-            0,
-            lowScreen.innerText.length - 1
-          )
-        }
-        if (lowScreen.innerText === '') {
-          lowScreen.innerText = '0'
-        }
-        break
-      case '=':
-        try {
-          evaluate(lowScreen.innerText)
-          screen.innerText = lowScreen.innerText
-          lowScreen.innerText = evaluate(lowScreen.innerText)
-        } catch (error) {
-          const cache = lowScreen.innerText
-          lowScreen.innerText = 'error'
-          lowScreen.classList.toggle('error')
-          setTimeout(() => {
-            lowScreen.innerText = cache
-            lowScreen.classList.toggle('error')
-          }, 700)
-        }
-        break
-      default:
-        console.error('Something went wrong!')
-        break
+    if (
+      lastCharactor === '*' ||
+      lastCharactor === '/' ||
+      lastCharactor === '+' ||
+      lastTwoCharactors === '--'
+    ) {
+      lowScreen.innerText = lowScreen.innerText.slice(
+        0,
+        lowScreen.innerText.length - 1
+      )
+      lowScreen.textContent += key
+      return
     }
-  })
-})
+    lowScreen.textContent += key
+    return
+  }
+}
